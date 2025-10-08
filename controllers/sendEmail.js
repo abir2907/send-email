8b79a9ebb5e21632a215361cdeb76194fc44ef3a
Abir Chodha <abirchodha1@gmail.com>
2025-10-08 16:05:43 +0530

    Send emails (dev) using nodemailer and ethereal

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,25 @@
+const nodemailer = require("nodemailer");
+
 const sendEmail = async (req, res) => {
-  res.send("send email");
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "norberto.adams@ethereal.email",
+      pass: "8DedzWQf5g8A2d64BN",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Abir Chodha" <abirchodha1@gmail.com>',
+    to: "abirchodha@gmail.com",
+    subject: "Hello",
+    html: "<h2>Sending Emails with Node.js</h2>",
+  });
+
+  res.json(info);
 };
 
 module.exports = sendEmail;
